ed5e4897b32b00b6c0faff8ed697b35513e58834
Devesh Krishna Mishra <mdevesh47@gmail.com>
2025-11-19 01:40:03 +0530

    update eslint ignore rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "dist/**",
+      "src/generated/**",
+      "public/**",
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
