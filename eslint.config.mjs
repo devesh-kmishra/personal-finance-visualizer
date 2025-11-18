ef99ebe9de73271e15c1725a4d22d19b846ca1d9
Devesh Krishna Mishra <mdevesh47@gmail.com>
2025-11-19 01:23:41 +0530

    update eslint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,11 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = defineConfig([
+const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...nextVitals,
-  ...nextTs,
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-]);
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+];
 
 export default eslintConfig;
